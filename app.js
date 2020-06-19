c2f7c6d21ecf9f4f363b59f2edb8055f388e176b
gosteve <nomuojamaro@gmail.com>
2020-06-19 14:20:03 +0100

    increased security to use hashing instead of encryption

--- app.js
+++ app.js
@@ -2,10 +2,11 @@
 require('dotenv').config();
 const express = require("express");
 const ejs = require("ejs");
+
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static('public'));
@@ -20,7 +21,7 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields:['password'] });
+
 const User = new mongoose.model('user', userSchema);
 
 
@@ -39,7 +40,7 @@ app.get('/login',function(req,res){
 
 app.post('/register', function(req,res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   const user = new User({
     email: username,
@@ -65,7 +66,7 @@ app.post('/login',function(req,res){
     if (err){
       console.log(err);
     }else{
-      if(foundUser.password === password){
+      if(foundUser.password === md5(password)){
         res.render('secrets');
       }else{
         res.send('wrong password or username');
